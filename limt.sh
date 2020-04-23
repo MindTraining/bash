da00ce9fdc9ec0d5883481386f5caa68656db068
qingliang <qingliang>
2020-04-23 14:10:11 +0800

    根据机器使用条件限制用户使用

---
+++ limt.sh
@@ -0,0 +1,7 @@
+# !/bin/sh 
+PIDS=`top -bn 1 | grep "^ *[1-9]" | awk ¡¯{ if($9 > 200 || $10 > 25 && id -u $2 > 500) print $1}¡¯`
+for PID in $PIDS
+do
+        renice +10 $PID
+        echo "renice +10 $PID"
+done
